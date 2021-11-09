8cff9adda76ef0e8cfb360dc2fe2ed3e9f94ac9b
Y0ni0 <eshcolsam@gmail.com>
2021-11-09 15:06:23 +0300

    scope chain

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ function calculateAge(year){
 var retirement = function(year){
     console.log(65-(2021-year));
 }
-retirement(1998);
+retirement(1998); 
+
+var a = 'Hello';
+first();
+function first(){
+    var b ='hi';
+    second();
+    function second(){
+        var c = 'dummy';
+        third();
+        function third(){
+            var d = 'john';
+            console.log(a+b+c+d);
+        }
+    }
+}
