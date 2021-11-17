2574edd0a1926b9ccefd80b8a9f22c852e647ea8
Y0ni0 <eshcolsam@gmail.com>
2021-11-17 14:09:47 +0300

    game rules

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
-console.log('hello world');
+/* Game Rules
+
+-The game has 2 players, playing in rounds 
+-In each turn, a player rolls a dice as many times as he wishes. Each result gets added to his ROUND SCORE
+-But, if the player rolles a 1, all his ROOND score gets lost. After that, it's the next players turn 
+-the player can choose to 'hold', which means that his Round score gets added to his GLOBAL score. 
+after the, its's the next players turn
+-The first player to reach 100 points on GLOBAL score wins the game
+*/
